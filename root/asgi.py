38f1b54baf41dcde254058da377add00e7ccc745
goldendevuz <goldendevuz@gmail.com>
2026-02-07 16:01:42 +0500

    2026.02.07/16:01

--- root/asgi.py
+++ root/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
 
 application = get_asgi_application()
